36b2737f5e932656127b80a2a5fb852097b57489
Daniël Louwrink <daniel.louwrink@gmail.com>
2019-06-17 18:25:36 +0200

    update plusn0 example to use builtin types

--- common/src/Lib.hs
+++ common/src/Lib.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( run
+    , doFile
     ) where
 
 import Data.Text (Text)
@@ -8,6 +9,7 @@ import Parser (parseTerm)
 import PrettyPrint (prettyPrint)
 import Normalize (normalize)
 import TypeCheck (typeCheck)
+import qualified Data.Text.IO as TIO
 
 run :: Text -> (Text, Text)
 run source = case parsed of
@@ -16,3 +18,11 @@ run source = case parsed of
     Left err -> ("type error: " <> err, "")
     Right ty -> ("Type: " <> prettyPrint (normalize ty), "Normalized: " <> prettyPrint (normalize t))
   where parsed = parseTerm source
+
+doFile :: String -> IO ()
+doFile filename = do
+  source <- TIO.readFile filename
+  let (a, b) = run source
+  TIO.putStrLn a
+  TIO.putStrLn ""
+  TIO.putStrLn b
